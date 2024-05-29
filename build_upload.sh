0ecb09be8f10baa7f32c3e899c6d53f8fb69a47c
Vincenzo <vincenzo.timmel@fhnw.ch>
2024-05-29 16:46:12 +0200

    Bump version.

--- build_upload.sh
+++ build_upload.sh
@@ -7,4 +7,4 @@ echo "Building the package..."
 python -m build
 
 echo "Uploading the package to PyPi..."
-python -m twine upload --repository pypi dist/*
+python -m twine upload --repository pypi dist/* --verbose
